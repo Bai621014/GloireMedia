71a4573f7d6d193e07f89b61c297dfa0e3a665fe
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-06-23 01:08:28 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className="bg-black text-white">
+      <body>
         {children}
       </body>
     </html>
