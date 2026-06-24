bb819f3f8319f4b36461a14eb597809b92a2a832
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-06-24 21:51:26 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import UserProfile from './UserProfile';
-// Note: nous importerons le CSS ici une fois créé
-import './style.css'; 
+import './style.css';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <UserProfile initialBalance={1250} />
+      {/* On laisse UserProfile aller chercher ses données seul dans Supabase */}
+      <UserProfile />
     </React.StrictMode>
   );
 } else {
-  console.error("L'élément root est introuvable !");
+  console.error("Erreur critique : L'élément avec l'ID 'root' est introuvable dans le HTML.");
 }
