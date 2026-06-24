20492bd8a6de8933cbdad7a77120b08e357d0ec2
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-06-24 10:06:32 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import UserProfile from '../UserProfile'; // Ajustez le chemin selon où est UserProfile.js
+import UserProfile from './UserProfile'; // Chemin corrigé : le "./" signifie "dans le dossier actuel"
 import './style.css';
 
+// Rendu de l'application dans la div avec l'id "root" qui se trouve dans index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProfile initialBalance={1250} />
