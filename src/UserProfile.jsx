91d87258190be3e1be6fd1a623a433402d8b7d01
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-06-24 22:27:24 +0100

    Update UserProfile.jsx

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -1,7 +1,7 @@
 export default function UserProfile() {
   return (
-    <div style={{ color: 'white', padding: '50px', fontSize: '24px', textAlign: 'center' }}>
-      <h1>GloireMedia est enfin visible !</h1>
+    <div style={{ color: "white", padding: "50px", fontSize: "30px" }}>
+      Test d'affichage réussi !
     </div>
   );
 }
